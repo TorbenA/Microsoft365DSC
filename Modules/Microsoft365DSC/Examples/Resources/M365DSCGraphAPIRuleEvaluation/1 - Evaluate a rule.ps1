987af04f17e5acb11ddcecd9414c36a2dc18ea99
Nik Charlebois <nicharl@microsoft.com>
2025-08-28 08:27:24 -0400

    M365DSCGraphAPIRuleEvaluation Initial Release

---
+++ Modules/Microsoft365DSC/Examples/Resources/M365DSCGraphAPIRuleEvaluation/1 - Evaluate a rule.ps1
@@ -0,0 +1,26 @@
+<#
+This example is used to test new resources and showcase the usage of new resources being worked on.
+It is not meant to use as a production baseline.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $CredsCredential
+    )
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    node localhost
+    {
+        M365DSCGraphAPIRuleEvaluation 'AllowAnonymousUsersToJoinMeetingAllPolicies'
+        {
+            APIUrl            = 'https://graph.microsoft.com/beta/serviceprincipals'
+            InstancesProperty = 'value'
+            RuleDefinition    = "`$_.appCategory -eq 'mdm'"
+            AfterRuleCount    = '-eq 4'
+            Credential        = $CredsCredential
+        }
+    }
+}
