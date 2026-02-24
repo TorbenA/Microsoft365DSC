a88ee4b403efba4bbeb2ece54cbdef19722fbdb1
Mike Poulson <mike@mikepoulson.com>
2026-02-23 19:20:05 -0800

    Add CertificateBasedApplicationConfigurationIds support in AADTenantAppManagementPolicy

--- Modules/Microsoft365DSC/DSCResources/MSFT_AADTenantAppManagementPolicy/MSFT_AADTenantAppManagementPolicy.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_AADTenantAppManagementPolicy/MSFT_AADTenantAppManagementPolicy.psm1
@@ -115,6 +115,10 @@ function Get-TargetResource
                 $iso8601Duration = "P{0}DT{1}H{2}M{3}S" -f $keyCred.MaxLifetime.Days, $keyCred.MaxLifetime.Hours, $keyCred.MaxLifetime.Minutes, $keyCred.MaxLifetime.Seconds
                 $newItem.Add('maxLifetime', $iso8601Duration)
             }
+            if ($null -ne $keyCred.CertificateBasedApplicationConfigurationIds -and $keyCred.CertificateBasedApplicationConfigurationIds.Count -gt 0)
+            {
+                $newItem.Add('certificateBasedApplicationConfigurationIds', [System.String[]]$keyCred.CertificateBasedApplicationConfigurationIds)
+            }
             $appRestrictionsValue.keyCredentials += $newItem
         }
         #endregion
@@ -151,6 +155,10 @@ function Get-TargetResource
             {
                 $iso8601Duration = "P{0}DT{1}H{2}M{3}S" -f $keyCred.MaxLifetime.Days, $keyCred.MaxLifetime.Hours, $keyCred.MaxLifetime.Minutes, $keyCred.MaxLifetime.Seconds
                 $newItem.Add('maxLifetime', $iso8601Duration)
+            }
+            if ($null -ne $keyCred.CertificateBasedApplicationConfigurationIds -and $keyCred.CertificateBasedApplicationConfigurationIds.Count -gt 0)
+            {
+                $newItem.Add('certificateBasedApplicationConfigurationIds', [System.String[]]$keyCred.CertificateBasedApplicationConfigurationIds)
             }
             $spnRestrictionsValue.keyCredentials += $newItem
         }
@@ -284,6 +292,10 @@ function Set-TargetResource
         {
             $newItem.Add('maxLifetime', $keyCred.MaxLifetime.ToString())
         }
+        if ($null -ne $keyCred.CertificateBasedApplicationConfigurationIds -and $keyCred.CertificateBasedApplicationConfigurationIds.Count -gt 0)
+        {
+            $newItem.Add('certificateBasedApplicationConfigurationIds', [System.String[]]$keyCred.CertificateBasedApplicationConfigurationIds)
+        }
         $appRestrictionsValue.keyCredentials += $newItem
     }
 
@@ -318,6 +330,10 @@ function Set-TargetResource
         if ($null -ne $keyCred.MaxLifetime)
         {
             $newItem.Add('maxLifetime', $keyCred.MaxLifetime.ToString())
+        }
+        if ($null -ne $keyCred.CertificateBasedApplicationConfigurationIds -and $keyCred.CertificateBasedApplicationConfigurationIds.Count -gt 0)
+        {
+            $newItem.Add('certificateBasedApplicationConfigurationIds', [System.String[]]$keyCred.CertificateBasedApplicationConfigurationIds)
         }
         $spnRestrictionsValue.keyCredentials += $newItem
     }
