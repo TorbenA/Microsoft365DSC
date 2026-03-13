750c2892e1dcb542434054c03bd683c9c89f534e
Nik Charlebois <nicharl@microsoft.com>
2026-03-13 15:19:47 -0400

    Fixes

--- Tests/Unit/Microsoft365DSC/Microsoft365DSC.DefenderRoleDefinition.Tests.ps1
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.DefenderRoleDefinition.Tests.ps1
@@ -35,8 +35,6 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
                 return "Credentials"
             }
 
-            ##TODO - Mock any Remove/Set/New cmdlets
-
             # Mock Write-Host to hide output during the tests
             Mock -CommandName Write-Host -MockWith {
             }
@@ -47,14 +45,26 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
         Context -Name "The instance should exist but it DOES NOT" -Fixture {
             BeforeAll {
                 $testParams = @{
-                    ##TODO - Add Parameters
-                    Ensure              = 'Present'
+                    Description           = "Testing";
+                    DisplayName           = "MyRoleDefinition";
+                    Ensure                = "Present";
+                    RolePermissions       = @(
+                        (New-CIMInstance -ClassName MSFT_DefenderRoleDefinitionRolePermissions -Property @{
+                            allowedResourceActions = @(
+                                "microsoft.xdr/secops/*/manage"
+                                "microsoft.xdr/securityposture/*/manage"
+                                "microsoft.xdr/configuration/*/manage"
+                            )
+                        } -ClientOnly)
+                    );
                     Credential          = $Credential;
                 }
 
-                ##TODO - Mock the Get-Cmdlet to return $null
-                Mock -CommandName Get-Cmdlet -MockWith {
-                    return $null
+
+                Mock -CommandName Invoke-MgGraphRequest -MockWith {
+                    return @{
+                        value = $null
+                    }
                 }
             }
             It 'Should return Values from the Get method' {
@@ -65,24 +75,46 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
             }
 
             It 'Should create a new instance from the Set method' {
-                ##TODO - Replace the New-Cmdlet by the appropriate one
                 Set-TargetResource @testParams
-                Should -Invoke -CommandName New-Cmdlet -Exactly 1
+                Should -Invoke -CommandName Invoke-MgGraphRequest -Exactly 2
             }
         }
 
         Context -Name "The instance exists but it SHOULD NOT" -Fixture {
             BeforeAll {
                 $testParams = @{
-                    ##TODO - Add Parameters
-                    Ensure              = 'Absent'
+                    Description           = "Testing";
+                    DisplayName           = "MyRoleDefinition";
+                    Ensure                = "Absent";
+                    RolePermissions       = @(
+                        (New-CIMInstance -ClassName MSFT_DefenderRoleDefinitionRolePermissions -Property @{
+                            allowedResourceActions = @(
+                                "microsoft.xdr/secops/*/manage"
+                                "microsoft.xdr/securityposture/*/manage"
+                                "microsoft.xdr/configuration/*/manage"
+                            )
+                        } -ClientOnly)
+                    );
                     Credential          = $Credential;
                 }
 
-                ##TODO - Mock the Get-Cmdlet to return an instance
-                Mock -CommandName Get-Cmdlet -MockWith {
+                Mock -CommandName Invoke-MgGraphRequest -MockWith {
                     return @{
-
+                        value = @(
+                            @{
+                                displayName = 'MyRoleDefinition'
+                                description = 'Testing'
+                                rolePermissions = @(
+                                    @{
+                                        allowedResourceActions = @(
+                                            "microsoft.xdr/secops/*/manage",
+                                            "microsoft.xdr/securityposture/*/manage",
+                                            "microsoft.xdr/configuration/*/manage"
+                                        )
+                                    }
+                                )
+                            }
+                        )
                     }
                 }
             }
@@ -95,23 +127,45 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
 
             It 'Should remove the instance from the Set method' {
                 Set-TargetResource @testParams
-                ##TODO - Replace the Remove-Cmdlet by the appropriate one
-                Should -Invoke -CommandName Remove-Cmdlet -Exactly 1
+                Should -Invoke -CommandName Invoke-MgGraphRequest -Exactly 2
             }
         }
 
         Context -Name "The instance exists and values are already in the desired state" -Fixture {
             BeforeAll {
                 $testParams = @{
-                    ##TODO - Add Parameters
-                    Ensure              = 'Present'
+                    Description           = "Testing";
+                    DisplayName           = "MyRoleDefinition";
+                    Ensure                = "Present";
+                    RolePermissions       = @(
+                        (New-CIMInstance -ClassName MSFT_DefenderRoleDefinitionRolePermissions -Property @{
+                            allowedResourceActions = @(
+                                "microsoft.xdr/secops/*/manage"
+                                "microsoft.xdr/securityposture/*/manage"
+                                "microsoft.xdr/configuration/*/manage"
+                            )
+                        } -ClientOnly)
+                    );
                     Credential          = $Credential;
                 }
 
-                ##TODO - Mock the Get-Cmdlet to return the desired values
-                Mock -CommandName Get-Cmdlet -MockWith {
+                Mock -CommandName Invoke-MgGraphRequest -MockWith {
                     return @{
-
+                        value = @(
+                            @{
+                                displayName = 'MyRoleDefinition'
+                                description = 'Testing'
+                                rolePermissions = @(
+                                    @{
+                                        allowedResourceActions = @(
+                                            "microsoft.xdr/secops/*/manage",
+                                            "microsoft.xdr/securityposture/*/manage",
+                                            "microsoft.xdr/configuration/*/manage"
+                                        )
+                                    }
+                                )
+                            }
+                        )
                     }
                 }
             }
@@ -124,15 +178,38 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
         Context -Name "The instance exists and values are NOT in the desired state" -Fixture {
             BeforeAll {
                 $testParams = @{
-                    ##TODO - Add Parameters
-                    Ensure              = 'Present'
+                    Description           = "Testing";
+                    DisplayName           = "MyRoleDefinition";
+                    Ensure                = "Present";
+                    RolePermissions       = @(
+                        (New-CIMInstance -ClassName MSFT_DefenderRoleDefinitionRolePermissions -Property @{
+                            allowedResourceActions = @(
+                                "microsoft.xdr/secops/*/manage"
+                                "microsoft.xdr/securityposture/*/manage"
+                                "microsoft.xdr/configuration/*/manage"
+                            )
+                        } -ClientOnly)
+                    );
                     Credential          = $Credential;
                 }
 
-                ##TODO - Mock the Get-Cmdlet to return a drift
-                Mock -CommandName Get-Cmdlet -MockWith {
+                Mock -CommandName Invoke-MgGraphRequest -MockWith {
                     return @{
-
+                        value = @(
+                            @{
+                                displayName = 'MyRoleDefinition'
+                                description = 'Testing-Different'
+                                rolePermissions = @(
+                                    @{
+                                        allowedResourceActions = @(
+                                            "microsoft.xdr/secops/*/manage",
+                                            "microsoft.xdr/securityposture/*/manage",
+                                            "microsoft.xdr/configuration/*/manage"
+                                        )
+                                    }
+                                )
+                            }
+                        )
                     }
                 }
             }
@@ -147,8 +224,7 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
 
             It 'Should call the Set method' {
                 Set-TargetResource @testParams
-                ##TODO - Replace the Update-Cmdlet by the appropriate one
-                Should -Invoke -CommandName Update-Cmdlet -Exactly 1
+                Should -Invoke -CommandName Invoke-MgGraphRequest -Exactly 2
             }
         }
 
@@ -160,10 +236,23 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
                     Credential  = $Credential;
                 }
 
-                ##TODO - Mock the Get-Cmdlet to return an instance
-                Mock -CommandName Get-Cmdlet -MockWith {
+                Mock -CommandName Invoke-MgGraphRequest -MockWith {
                     return @{
-
+                        value = @(
+                            @{
+                                displayName = 'MyRoleDefinition'
+                                description = 'Testing'
+                                rolePermissions = @(
+                                    @{
+                                        allowedResourceActions = @(
+                                            "microsoft.xdr/secops/*/manage",
+                                            "microsoft.xdr/securityposture/*/manage",
+                                            "microsoft.xdr/configuration/*/manage"
+                                        )
+                                    }
+                                )
+                            }
+                        )
                     }
                 }
             }
