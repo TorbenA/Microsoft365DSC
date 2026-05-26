506539b61cba6c2e65e1be8879afb9286f69a72f
Fabien Tschanz <fabien.tschanz@bluewin.ch>
2026-05-26 13:18:47 +0200

    Add Teams Notification and Feeds Policy + Targeting Policy

--- Modules/Microsoft365DSC/DSCResources/MSFT_TeamsNotificationAndFeedsPolicy/MSFT_TeamsNotificationAndFeedsPolicy.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_TeamsNotificationAndFeedsPolicy/MSFT_TeamsNotificationAndFeedsPolicy.psm1
@@ -4,16 +4,65 @@ function Get-TargetResource
     [OutputType([System.Collections.Hashtable])]
     param
     (
-<ParameterBlock>
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Yes')]
+        [System.String]
+        $IsSingleInstance,
+
+        [Parameter()]
+        [System.String]
+        $Description,
+
+        [Parameter()]
+        [ValidateSet('Disabled', 'EnabledUserOverride')]
+        [System.String]
+        $SuggestedFeedsEnabledType,
+
+        [Parameter()]
+        [ValidateSet('Disabled', 'EnabledUserOverride')]
+        [System.String]
+        $TrendingFeedsEnabledType,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [System.String]
+        $CertificatePath,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $CertificatePassword,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity,
+
+        [Parameter()]
+        [System.String[]]
+        $AccessTokens
     )
 
-    Write-Verbose -Message "Getting configuration for <ResourceDescription> with <FilterKey> {$<FilterKey>}."
+    Write-Verbose -Message "Getting configuration for Teams Notification And Feeds Policy with Identity {$Identity}."
 
     try
     {
-        if (-not $Script:exportedInstance -or $Script:exportedInstance.<PrimaryKey> -ne $<PrimaryKey>)
+        if (-not $Script:exportedInstance -or $Script:exportedInstance.Identity -ne $Identity)
         {
-            $null = New-M365DSCConnection -Workload '<#Workload#>' `
+            $null = New-M365DSCConnection -Workload 'MicrosoftTeams' `
                 -InboundParameters $PSBoundParameters
 
             #Ensure the proper dependencies are installed in the current environment.
@@ -31,11 +80,10 @@ function Get-TargetResource
             $nullResult = $PSBoundParameters
             $nullResult.Ensure = 'Absent'
 
-            $instance = <GetCmdLetName> -<getKeyIdentifier> $<PrimaryKey> -ErrorAction SilentlyContinue
+            $instance = Get-CsTeamsNotificationAndFeedsPolicy -Identity 'Global' -ErrorAction Stop
             if ($null -eq $instance)
             {
-                Write-Verbose -Message "Could not find an <ResourceDescription> with <FilterKey> {$<FilterKey>}."
-                return $nullResult
+                throw 'Could not find a Teams Notification And Feeds Policy with Identity {Global}.'
             }
         }
         else
@@ -43,9 +91,20 @@ function Get-TargetResource
             $instance = $Script:exportedInstance
         }
 
-        Write-Verbose -Message "An <ResourceDescription> with <PrimaryKey> {$<PrimaryKey>} and <FilterKey> {$<FilterKey>} was found"
+        Write-Verbose -Message "A Teams Notification And Feeds Policy with Identity {Global} was found"
         $results = @{
-<HashTableMapping>        }
+            IsSingleInstance          = 'Yes'
+            Description               = $instance.Description
+            SuggestedFeedsEnabledType = $instance.SuggestedFeedsEnabledType
+            TrendingFeedsEnabledType  = $instance.TrendingFeedsEnabledType
+            Credential                = $Credential
+            ApplicationId             = $ApplicationId
+            TenantId                  = $TenantId
+            CertificateThumbprint     = $CertificateThumbprint
+            CertificatePath           = $CertificatePath
+            CertificatePassword       = $CertificatePassword
+            ManagedIdentity           = $ManagedIdentity.IsPresent
+        }
         return $results
     }
     catch
@@ -65,10 +124,59 @@ function Set-TargetResource
     [CmdletBinding()]
     param
     (
-<ParameterBlock>
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Yes')]
+        [System.String]
+        $IsSingleInstance,
+
+        [Parameter()]
+        [System.String]
+        $Description,
+
+        [Parameter()]
+        [ValidateSet('Disabled', 'EnabledUserOverride')]
+        [System.String]
+        $SuggestedFeedsEnabledType,
+
+        [Parameter()]
+        [ValidateSet('Disabled', 'EnabledUserOverride')]
+        [System.String]
+        $TrendingFeedsEnabledType,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [System.String]
+        $CertificatePath,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $CertificatePassword,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity,
+
+        [Parameter()]
+        [System.String[]]
+        $AccessTokens
     )
 
-    $null = New-M365DSCConnection -Workload '<#Workload#>' `
+    $null = New-M365DSCConnection -Workload 'MicrosoftTeams' `
         -InboundParameters $PSBoundParameters
 
     #Ensure the proper dependencies are installed in the current environment.
@@ -86,25 +194,12 @@ function Set-TargetResource
     $currentInstance = Get-TargetResource @PSBoundParameters
     $boundParameters = Remove-M365DSCAuthenticationParameter -BoundParameters $PSBoundParameters
 
-    if ($Ensure -eq 'Present' -and $currentInstance.Ensure -eq 'Absent')
-    {
-        $createParameters = ([Hashtable]$boundParameters).Clone()
+    Write-Verbose -Message "Updating a Teams Notification And Feeds Policy with Identity {Global}"
 
-        Write-Verbose -Message "Creating an <ResourceDescription> with <FilterKey> {$<PrimaryKey>}"
-        <NewCmdLetName> @createParameters | Out-Null
-    }
-    elseif ($Ensure -eq 'Present' -and $currentInstance.Ensure -eq 'Present')
-    {
-        Write-Verbose -Message "Updating an <ResourceDescription> with <FilterKey> {$<PrimaryKey>}"
-
-        $updateParameters = ([Hashtable]$boundParameters).Clone()
-        <UpdateCmdLetName> @updateParameters | Out-Null
-    }
-    elseif ($Ensure -eq 'Absent' -and $currentInstance.Ensure -eq 'Present')
-    {
-        Write-Verbose -Message "Removing the <ResourceDescription> with <PrimaryKey> {$($currentInstance.<PrimaryKey>)}"
-        <RemoveCmdLetName> -<PrimaryKey> $currentInstance.<PrimaryKey>
-    }
+    $updateParameters = ([Hashtable]$boundParameters).Clone()
+    $updateParameters.Remove('IsSingleInstance') | Out-Null
+    $updateParameters.Add('Identity', 'Global')
+    Set-CsTeamsNotificationAndFeedsPolicy @updateParameters | Out-Null
 }
 
 function Test-TargetResource
@@ -113,7 +208,56 @@ function Test-TargetResource
     [OutputType([System.Boolean])]
     param
     (
-<ParameterBlock>
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Yes')]
+        [System.String]
+        $IsSingleInstance,
+
+        [Parameter()]
+        [System.String]
+        $Description,
+
+        [Parameter()]
+        [ValidateSet('Disabled', 'EnabledUserOverride')]
+        [System.String]
+        $SuggestedFeedsEnabledType,
+
+        [Parameter()]
+        [ValidateSet('Disabled', 'EnabledUserOverride')]
+        [System.String]
+        $TrendingFeedsEnabledType,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [System.String]
+        $CertificatePath,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $CertificatePassword,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity,
+
+        [Parameter()]
+        [System.String[]]
+        $AccessTokens
     )
 
     #region Telemetry
@@ -173,7 +317,7 @@ function Export-TargetResource
         $AccessTokens
     )
 
-   $ConnectionMode = New-M365DSCConnection -Workload '<#Workload#>' `
+   $ConnectionMode = New-M365DSCConnection -Workload 'MicrosoftTeams' `
         -InboundParameters $PSBoundParameters
 
     #Ensure the proper dependencies are installed in the current environment.
@@ -190,7 +334,7 @@ function Export-TargetResource
 
     try
     {
-        [array]$getValue = <GetCmdLetName> -ErrorAction Stop
+        [array]$getValue = Get-CsTeamsNotificationAndFeedsPolicy -Identity 'Global' -ErrorAction Stop
 
         $i = 1
         $dscContent = [System.Text.StringBuilder]::new()
@@ -209,17 +353,13 @@ function Export-TargetResource
                 $Global:M365DSCExportResourceInstancesCount++
             }
 
-            $displayedKey = $config.<PrimaryKey>
-            if (-not [System.String]::IsNullOrEmpty($config.displayName))
-            {
-                $displayedKey = $config.displayName
-            }
+            $displayedKey = $config.Identity
             Write-M365DSCHost -Message "    |---[$i/$($getValue.Count)] $displayedKey" -DeferWrite
             $params = @{
-                <ExportParams>
+                IsSingleInstance      = 'Yes'
                 Credential            = $Credential
                 ApplicationId         = $ApplicationId
-                TenantId              = $TenantId<ApplicationSecret>
+                TenantId              = $TenantId
                 CertificateThumbprint = $CertificateThumbprint
                 CertificatePath       = $CertificatePath
                 CertificatePassword   = $CertificatePassword
